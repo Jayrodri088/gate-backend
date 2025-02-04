421132838f39006831c4f49f32d2e791c4095db6
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-02-04 15:36:14 +0100

    test

--- status_update.php
+++ status_update.php
@@ -37,7 +37,7 @@ $stmt->execute();
 if ($stmt->fetchColumn() == 0) {
     echo json_encode([
         'success' => false,
-        'message' => 'Record not found',
+        'message' => ['id_number' => $idNumber, 'extracted_id' => $id],
         'debug' => ['id_number' => $idNumber, 'extracted_id' => $id]
     ]);
     exit();
